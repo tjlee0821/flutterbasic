09f485f37db7aa3b1000d8ce6628668198dd610a
Taeryong Lee <teo0821@gmail.com>
2023-07-24 16:16:19 +0900

    main_1

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,29 @@ import 'package:flutter/material.dart';
 void main(){
   runApp(
     MaterialApp(
-    home: const Scaffold(
-      body:SafeArea(
-        child: const Text('Hello, Flutter', style: TextStyle(
-          color:Colors.black,
-        ),
-        ),
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text('Container'),
       ),
-    ),
+      body: const CustomContainer(),
+  ),
   ),
   );
+}
+
+class CustomContainer extends StatelessWidget {
+  const CustomContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width:double.infinity,
+      height:double.infinity,
+      //color:Colors.red.shade200,
+      color:const Color(0xFF85D07B),
+      padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+      margin: EdgeInsets.symmetric(vertical: 24, horizontal: 30),
+      child:Center(child: Text('Hello, Flutter'),),
+    );
+  }
 }
