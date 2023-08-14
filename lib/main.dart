349cf21931629d39118a26f012b113e5fc735f77
Taeryong Lee <teo0821@gmail.com>
2023-08-14 11:55:06 +0900

    flutter_input

--- lib/main.dart
+++ lib/main.dart
@@ -6,44 +6,23 @@ void main(){
         title: const Text('widget 배율로 배치'),
       ),
 
-      body:const HomeWidget(),
+      body:const Body(),
     )
 
   ),
   );
 
 }
-class HomeWidget extends StatelessWidget {
-  const HomeWidget({super.key});
+
+class Body extends StatelessWidget {
+  const Body({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
-      child: Scaffold(
-        body:ConstraintWidget(),
-      )
-    );
-  }
-}
-
-class ConstraintWidget extends StatelessWidget {
-  const ConstraintWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height:500,width: 500, 
-      color:Colors.blue,
-      child: Center(
+    return const Column(
+      children: [
         
-        child: Container(
-          height:300,
-          width:300,
-          color:Colors.red,
-        ),
-      ),
-
-
+      ],
     );
   }
 }
---
+++ lib/main_widget_ratio.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+void main(){
+  runApp(MaterialApp(
+    home:Scaffold(
+      appBar: AppBar(
+        title: const Text('widget 배율로 배치'),
+      ),
+
+      body:const HomeWidget(),
+    )
+
+  ),
+  );
+
+}
+class HomeWidget extends StatelessWidget {
+  const HomeWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(
+      child: Scaffold(
+        body:ConstraintWidget(),
+      )
+    );
+  }
+}
+
+class ConstraintWidget extends StatelessWidget {
+  const ConstraintWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height:500,width: 500, 
+      color:Colors.blue,
+      child: Center(
+        
+        child: Container(
+          height:300,
+          width:300,
+          color:Colors.red,
+        ),
+      ),
+
+
+    );
+  }
+}
