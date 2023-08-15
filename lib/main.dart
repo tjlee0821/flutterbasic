fa3ccf01d2e75e95f0c3d36e66c3a9c003437013
Taeryong Lee <teo0821@gmail.com>
2023-08-15 10:02:31 +0900

    flutter_Random_words

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 void main(){
   runApp(MaterialApp(
     home:Scaffold(
       appBar: AppBar(
-        title: const Text('다양한 flutter의 call back event'),
+        title: const Text('Flutter 외부라이브러리'),
       ),
 
-      body:const Body(),
+      body:RandomWords(),
     )
 
   ),
@@ -14,71 +15,19 @@ void main(){
 
 }
 
-class Body extends StatelessWidget {
-  const Body({super.key});
+class RandomWords extends StatelessWidget {
+  const RandomWords
+({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return TestWidget();
-  }
-}
+    final wordList = generateWordPairs().take(5).toList();
+    
+    final widgets = wordList.map((word) => Text(word.asString, style: TextStyle(fontSize:32,),),).toList();
 
-class TestWidget extends StatefulWidget {
-  const TestWidget({super.key});
-
-  @override
-  State<TestWidget> createState() => _TestWidgetState();
-}
-
-class _TestWidgetState extends State<TestWidget> {
-int value = 0;
-
-  @override
-  Widget build(BuildContext context) {
+    
     return Column(
-      children: [
-        Center(
-          child: Text('Count : $value', style: const TextStyle(fontSize:30)),
-        ),
-        
-        TxtButton(addCount),
-      ],
-
-    );
-  }
-
-   void addCount(){
-    setState(() {
-      value ++;
-    });
-   }
-
-  //  void addCount( int addValue ){
-  //   setState(() {
-  //     value + addValue;
-  //   });
-  //  }
-}    
-
-
-class TxtButton extends StatelessWidget {
-  const TxtButton(this.callback, {super.key});
-
-  final VoidCallback callback;
-  // final Function(int) callback;
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap:()=> callback.call(),
-      //onTap:()=> callback.call(1),
-      child: Center(
-        child: Container(
-          margin: const EdgeInsets.symmetric(vertical:8),
-          width: double.infinity,
-          child: const Text('Tap Counter')
-          ),
-      ),
+      children: widgets
     );
   }
 }
