f12b7bf2b1f93ccc91f9f1e97a28c16c39fb433a
Taeryong Lee <teo0821@gmail.com>
2023-08-15 12:38:13 +0900

    Flutter 페이지 이동 상태에 따라서

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,63 @@
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
-const assetImageNetworkPath = 'https://www.collective-intelligent.com';
-const logoImage = '$assetImageNetworkPath/tmp/logo_256.png';
-void main(){
-  runApp(MaterialApp(
-    home:Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter 외부라이브러리'),
-      ),
+void main() => runApp(MyApp());
 
-      body:Body(),
-    )
-
-  ),
-  );
-
-}
-
-class Body extends StatelessWidget {
-  const Body({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Image.network('$logoImage')
-      );
+    return MaterialApp(
+      title: 'Flutter 이동',
+      home:HomeWidget(),
+    );
   }
 }
+
+class HomeWidget extends StatefulWidget {
+  const HomeWidget({super.key});
+
+  @override
+  State<HomeWidget> createState() => _HomeWidgetState();
+}
+
+class _HomeWidgetState extends State<HomeWidget> {
+  late int index;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    index = 0;
+  }
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:const Text('Flutter Page 이동 상태에 따라서'),
+      ),
+      body:homeBody(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'search'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'person'),
+
+        ],
+        currentIndex:index,
+        onTap:(newIndex) => setState(() {
+          index = newIndex;
+        }),
+    )
+    );
+  }
+
+  Widget homeBody(){
+    switch(index){
+      case 1:
+        return Center(child:Icon(Icons.home,size:100));
+      case 2:
+        return Center(child:Icon(Icons.search,size:100));
+      case 0:
+      default:
+        return Center(child:Icon(Icons.person,size:100));
+    }
+  }
+}
+
